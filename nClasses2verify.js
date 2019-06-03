aaa4256b7eb7a113bd95687ee2a864480b00c2c4
DEVOPS <51250753+nojrgp@users.noreply.github.com>
2019-06-03 11:40:02 -0500

    an JS update

---
+++ nClasses2verify.js
@@ -0,0 +1,22 @@
+//
+// jsc | jsshell REQUIRE constructor
+//
+class Dog{};
+
+class Cat{};
+
+class Hero{};
+
+class Car{};
+
+class Enemy{};
+
+class Game{
+    constructor(){};
+    one(){print('performing a complex taks...');};
+    two(){};
+    three(){};
+};
+
+
+var g= new Game();
